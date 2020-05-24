892363cf52d03638e59f5bbc9e36c76c9d512c88
cortneyg <gardner.cortney@yahoo.com>
2020-05-24 00:53:52 -0600

    fixed image
    fixed image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,13 +14,13 @@ function App() {
       />
       <Card
         name={contacts[1].name}
-        img={contacts[1].imgurl}
+        img={contacts[1].imgURL}
         tel={contacts[1].phone}
         email={contacts[1].email}
       />
       <Card
         name={contacts[2].name}
-        img={contacts[2].imgurl}
+        img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
       />
